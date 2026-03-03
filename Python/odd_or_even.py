030c685492e509693e16fcdf9f33dbe3f8464cec
Prathamesh-Patil-git <prathameshpatil9433@gmail.com>
2026-03-03 23:14:03 +0530

    odd_or_even done with bitmanipulation method

--- Python/odd_or_even.py
+++ Python/odd_or_even.py
@@ -1,7 +1,15 @@
 # common method for even or odd
 num = int(input("Enter number to check if it is even or odd : "))
 
-if num%2 == 0:
+# if num%2 == 0:
+#     print(num," : is even")
+# else:
+#     print(num," : is odd")
+
+# 2 . bit manipulation method
+
+if (num & 1) == 0:
     print(num," : is even")
 else:
     print(num," : is odd")
+
