8dc071c39978139a312907e9bfc1e79ee044950f
Prathamesh-Patil-git <prathameshpatil9433@gmail.com>
2026-03-05 11:27:15 +0530

    fibonacci_series done

---
+++ java_/Recursion/fibonacci_series.java
@@ -0,0 +1,15 @@
+public class fibonacci_series {
+    
+    public static int fib(int n){
+
+        if( n == 0 || n == 1 ) return n;
+
+        return fib(n-1) + fib(n-2);
+
+    }
+
+    public static void main(String[] args){
+
+        System.out.print(fib(5));
+    }
+}
