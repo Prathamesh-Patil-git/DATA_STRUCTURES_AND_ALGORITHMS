b6f0652ddebe7a5cd77e13f9e97865ef2df04c2f
Prathamesh-Patil-git <prathameshpatil9433@gmail.com>
2026-05-25 17:38:37 +0530

    selection sort done

---
+++ java_refresher/selection_sort.java
@@ -0,0 +1,37 @@
+public class selection_sort {
+    
+    public static void SelectionSort(int arr[]){
+
+        for(int i = 0; i< arr.length;i++){
+            int smallest = i;
+            for(int j = i+1; j<arr.length;j++){
+
+                if(arr[j] < arr[smallest]){
+                    smallest = j;
+                    
+                }
+            }
+
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void DisplayArr(int arr[]){
+
+        for(int val : arr){
+            System.out.print(val+ " ");
+        }
+    }
+
+    public static void main(String[] args) {
+     
+        int arr[] = {9,8,6,5,4};
+        DisplayArr(arr);
+        System.out.println();
+        SelectionSort(arr);
+        DisplayArr(arr);
+
+    }
+}
