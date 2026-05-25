3a9664f7adee2e22983fafddbace6ed74e3df9e9
Prathamesh-Patil-git <prathameshpatil9433@gmail.com>
2026-05-25 17:19:08 +0530

    optimized version skip if no swap

--- java_refresher/bubble_sort.java
+++ java_refresher/bubble_sort.java
@@ -1,28 +1,46 @@
+import java.util.*;
+
 public class bubble_sort {
-    
+
     public static void bubble__sort(int arr[]){
 
-        for(int j = 0; j<arr.length-1;j++){
-            for(int i = 0; i<arr.length-1-j;i++){
-                if(arr[i] > arr[i+1]){
-                    int temp = arr[i+1];
-                    arr[i+1] = arr[i];
-                    arr[i] = temp;
+        for(int i = 0; i< arr.length-1; i++){
+
+            boolean check = false; 
+
+            for(int j = 0 ; j<arr.length-1-i; j++){
+
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]= temp;
+
+                    check = true;
                 }
+            }
+
+            if(!check){
+                break;
             }
         }
     }
 
     public static void display_arr(int arr[]){
-        for(int val : arr){
+
+        for( int val : arr){
             System.out.print(val + " ");
         }
     }
 
-    public static void main(String[] args){
-        int arr[] = {5, 2, 8, 1};
-
+    public static void main(String[] args) {
+        
+        int arr[] = {4,3,2,1};
+        display_arr(arr);
+        System.out.println();
         bubble__sort(arr);
         display_arr(arr);
+
+
+
     }
 }
