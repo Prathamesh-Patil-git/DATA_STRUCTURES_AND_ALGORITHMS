3168152b1655309cac493aedbc23df3e76bdd610
Prathamesh-Patil-git <prathameshpatil9433@gmail.com>
2026-05-26 16:33:29 +0530

    strings practice rough work

---
+++ java_refresher/string_practice.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class string_practice {
+
+    
+    public static void main(String[] args) {
+        
+        
+        String st = "java";
+        String d = "java";
+
+        System.out.print(d.indexOf("a"));
+
+        
+    }
+}
